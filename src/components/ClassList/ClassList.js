b85fce0799b4c193705b956b08e5f7635862edbd
stuartErnstsen <stuarternstsen@gmail.com>
2021-01-12 14:44:14 -0700

    Completed lab and black diamond

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,31 @@
+import axios from 'axios';
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+      .then(res => {
+        this.setState({students: res.data})
+      })
+      .catch(err => console.log(err))
   }
 
   render() {
+    const studentList = this.state.students.map((e, i) => <Link key={i} to={`/student/${e.id}`}><h3>{e.first_name} {e.last_name}</h3></Link> )
     return (
       <div className="box">
-        <h1></h1>
+        <button onClick={this.props.history.goBack}>BACK TO HOME</button>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {studentList}
       </div>
     )
   }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Student extends Component {
   constructor() {
     super()
+    this.state = {
+      student: {}
+    }
+  }
 
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
+      .then(res => this.setState({student: res.data}))
+      .catch(err => console.log(err))
   }
 
   render() {
+    const {first_name, last_name, grade, email} = this.state.student
     return (
       <div className="box">
-        <h1>Student</h1>
+        <h1>Student:</h1>
+        <h1>{first_name} {last_name}</h1>
+        <h3>Grade: {grade}</h3>
+        <h3>Email: {email}</h3>
+        <button onClick={this.props.history.goBack}>BACK TO LIST</button>
       </div>
     )
   }
